ca6653953faa538860816c584a3cf04e525c9233
alejandromase <alejandromase94@gmail.com>
2025-05-18 18:45:14 +0200

    Actualizado correo

--- src/modules/main/components/home/ContactInfo.vue
+++ src/modules/main/components/home/ContactInfo.vue
@@ -18,7 +18,7 @@
       <ContactItem
         icon="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
         title="Email"
-        :lines="['martapelu@gmail.com']"
+        :lines="['peluqueria.marta.94@protonmail.com']"
       />
     </div>
   </div>
